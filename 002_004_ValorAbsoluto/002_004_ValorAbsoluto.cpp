9d7694719ebada03f9d99d3618d8e745207798b6
Flextus <benjasg10@gmail.com>
2024-04-11 22:11:01 -0600

    004_1104_Benjamin
    Se añadio el valor absoluto

---
+++ 002_004_ValorAbsoluto/002_004_ValorAbsoluto.cpp
@@ -0,0 +1,18 @@
+// 002_004_ValorAbsoluto.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	float num = 0;
+
+	std::cout << "Ingresa un numero" << std::endl;
+	std::cin >> num;
+		if (num < 0);
+		{
+			num = num*(-1);
+		}
+	std::cout << "Su valor absoluto es " << num << std::endl; 
+}
+
