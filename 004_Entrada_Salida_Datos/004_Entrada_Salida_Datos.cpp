5dc7d50eea20589dfc638acf40a72e4f3d917d24
Flextus <benjasg10@gmail.com>
2024-04-18 09:12:27 -0600

    003_1804_Benjamin
    Se inicio practica consultorio

---
+++ 004_Entrada_Salida_Datos/004_Entrada_Salida_Datos.cpp
@@ -0,0 +1,23 @@
+// 004_Entrada_Salida_Datos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+std::string NombreApellido = "a", EmpresadeSeguro = "a";
+bool alcohol = 0, tabaco = 0, drogas = 0;
+short int edad = 0, dedos = 0;
+float altura = 0, masa = 0;
+char MoF = 'a';
+
+
+
+    std::cout << "Bienvenido al Consultorio del Dr Sepulveda, ¿Seria tan amable de conestar mis preguntas?"<<std::endl;
+
+    std::cout << "¿Cual es su nombre?" << std::endl;
+    std::cin>> 
+
+}
+
