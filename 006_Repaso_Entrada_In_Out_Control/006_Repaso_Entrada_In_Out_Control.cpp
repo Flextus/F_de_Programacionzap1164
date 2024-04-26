b5fbf1f7d755947f179e4a724c9d665a9bf10517
Flextus <benjasg10@gmail.com>
2024-04-26 09:20:32 -0600

    001_2604_Benjamin
    Se continuo P6 hasta el primer switch case

--- 006_Repaso_Entrada_In_Out_Control/006_Repaso_Entrada_In_Out_Control.cpp
+++ 006_Repaso_Entrada_In_Out_Control/006_Repaso_Entrada_In_Out_Control.cpp
@@ -8,38 +8,51 @@
 int main()
 {
 std::string email, pass;
+short int menu = 0; 
 setlocale(LC_ALL, "es_MX.UTF-8");
 
     std::cout << "Bienvenido a Albion Online 2: Electric Boogalo" << std::endl;
-    std::cout << "Inicie Sesion para Continuar" << std::endl;
+    std::cout << "Inicie Sesión para Continuar" << std::endl;
     std::cout << "\t";
     std::cout << "\t";
     std::cout << "\t";
-    std::cout << "Inicie sesion usando el correo y contraseña que le fueron proporcionados previamente" << std::endl;
+    std::cout << "Inicie sesión usando el correo y contraseña que le fueron proporcionados previamente" << std::endl;
     std::cout << "Ingrese el email" << std::endl;
         std::cin >> email;
     std::cout<<"Ingrese la contraseña" << std::endl;
         std::cin >> pass;
     if (email=="albiondemobeta@hotmail.com")
-    {
+    {   if (pass=="demo123")
+        {
+        std::cout << "Cargando..." << std::endl;
+        std::cout << "...";
+        std::cout << "\t";
+        std::cout << "Seleccione una opción para continuar";
+        std::cout << "\t";
+            switch (menu)
+            {
+                case 1://Iniciar partida
+                    std::cout << "Albion online es un MMORPG no lineal donde escribes tu propia historia..." << std::endl;
+
+                case 2://Configuración
+                case 3://Changelog
+                case 4://Feedback & FAQ
+            
+
+            default:
+            break;
+
+            }
+
+        }
     }
     else
         {
-        std::cout << "Email invalido" << std::endl;
+        std::cout << "Informacion de inicio de sesion incorrecta" << std::endl;
         
         }
 
 
-/*std::cin >> email;
-if (email=="albiontest@hotmail.com")
-{
-    std::cout << "Ok" << std::endl;
 
-}
-else
-{
-    std::cout << "Pito, puto";
-    */
-}
 }
 
