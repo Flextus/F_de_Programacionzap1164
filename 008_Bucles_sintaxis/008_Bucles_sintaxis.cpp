f34f4a4dc4f418e39280173804f24a8876fba4fa
Flextus <benjasg10@gmail.com>
2024-05-13 09:50:01 -0600

    003_1305_Benjamin
    P08 iniciada

---
+++ 008_Bucles_sintaxis/008_Bucles_sintaxis.cpp
@@ -0,0 +1,23 @@
+// 008_Bucles_sintaxis.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+	bool nameg = true, namef = true;
+
+	std::cout << "Lets try the while" << std::endl;
+		while (nameg)
+		{
+			std::cout << "Your name is Geovanni Georgio?" << std::endl;
+			std::cin >> nameg;
+
+		}
+	std::cout << "then do while" << std::endl;
+		do 
+		{
+		
+		} 
+		while (true);
+}
