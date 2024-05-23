d43ca00e52837e4b57a19c44ee97827f09cfd96b
Flextus <benjasg10@gmail.com>
2024-05-23 08:06:51 -0600

    23_05_Benjamin
    P08 Done

--- 008_Bucles_sintaxis/008_Bucles_sintaxis.cpp
+++ 008_Bucles_sintaxis/008_Bucles_sintaxis.cpp
@@ -2,9 +2,13 @@
 //
 
 #include <iostream>
+#include <thread>   
+#include <chrono>
 
 int main()
 {
+short int delay = 1, delay2 = 2;
+	
 	bool nameg = true, namef = true;
 
 	std::cout << "Lets try the while" << std::endl;
@@ -14,10 +18,30 @@ int main()
 			std::cin >> nameg;
 
 		}
+
+	std::this_thread::sleep_for(std::chrono::seconds(delay2));
+	system("cls");
+
 	std::cout << "then do while" << std::endl;
+	short int abc = 0;
+	
 		do 
 		{
-		
+			abc = abc++;
+			std::cout << abc << std::endl;
+				std::this_thread::sleep_for(std::chrono::seconds(delay));
 		} 
-		while (true);
+		while (abc<=24);
+	std::this_thread::sleep_for(std::chrono::seconds(delay2));
+	system("cls");
+
+	
+	std::cout << "Now, for" << std::endl;
+	short int xyz = 100;
+
+	for (xyz; xyz>= 50; xyz--) //Cuando una variable se define dentro del "for", esta se resetea al valor inicializado siempre
+	{                          //Caso contrario, declararla fuera de este hace que el cambio de valor sea permanente
+		std::cout << xyz << std::endl;
+	}
+
 }
