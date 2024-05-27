914f580285c2ebe3c289715dbc636ea06e1ad4d6
Flextus <benjasg10@gmail.com>
2024-05-27 07:46:54 -0600

    003_2705_Benjamin
    p013 done

---
+++ 013_login_again/013_login_again.cpp
@@ -0,0 +1,27 @@
+// 013_logindenuevo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string.h>
+
+int main()
+{
+	//Password= hola123
+	std::string pass;
+
+	do
+	{
+
+		std::cout << "Hola, ingresa tu clave de Acceso" << std::endl;
+		std::cin >> pass;
+
+	} while (pass == "hola123");
+
+
+
+
+
+
+}
+
+
