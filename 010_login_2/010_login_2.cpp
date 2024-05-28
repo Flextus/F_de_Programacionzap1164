0b8c6daf2fc7e343b660830252942d9c1c0acb6e
Flextus <benjasg10@gmail.com>
2024-05-28 00:01:41 -0600

    001_2805_benjamin
    p010 done

--- 010_login_2/010_login_2.cpp
+++ 010_login_2/010_login_2.cpp
@@ -1,48 +1,71 @@
 // 010_login_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <cctype>
+#include <limits>
 
-int main()
-{
-	//Password = holaclave, num acceso = 2785
-	std::string pass;
-	int pin = 0;
-	bool checknum = 0;
-
-	do 
-	{
-		std::cout << "Hola, ingresa tu clave de Acceso" << std::endl;
-
-		std::cin >> pass;
-
-		std::cout << "Hola, ingresa tu PIN de Acceso" << std::endl;
-
-		do
-		{
-			std::cin >> pin;
-			checknum = std::cin.fail();
-
-			if (checknum)
-			{
-				std::cout << "La entrada no es un Valor valido, Ingresa u valor Valido" << std::endl;
-			}
-		} while (checknum == true);
-
-
-		if (pass == "holaclave" && pin == 2785)
-		{
-			std::cout << "Login exitoso\n";
-		}
-		else
-		{
-			std::cout << "Login fallido\n";
-		}
-	} while (true);
-		
-
-
-
-
-
+bool esCadenaValida(const std::string& cadena) {
+    for (char c : cadena) {
+        if (std::isdigit(c)) {
+            return false; // Si encontramos un dígito, la cadena no es válida
+        }
+    }
+    return true;
 }
+
+int main() 
+{
+    do 
+    {
+
+    // Password = holaclave, num acceso = 2785
+    std::string pass;
+    int pin = 0;
+    bool checkchar = false, checknum = false;
+
+    // Validación de la contraseña
+    do {
+        std::cout << "Hola, ingresa tu clave de Acceso: ";
+        std::getline(std::cin, pass);
+
+        if (esCadenaValida(pass)) {
+            checkchar = true;
+        }
+        else {
+            std::cout << "Entrada no valida. Por favor, Usa solo letras " << std::endl;
+        }
+    } while (!checkchar);
+
+    // Validación del PIN
+    do {
+        std::cout << "Hola, ingresa tu PIN de Acceso: ";
+        std::cin >> pin;
+
+        if (std::cin.fail()) {
+            std::cin.clear(); // Limpiar el estado de error
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignorar la entrada incorrecta
+            std::cout << "Entrada no valida. Por favor, usa solo numeros" << std::endl;
+        }
+        else {
+            checknum = true;
+        }
+    } while (!checknum);
+
+    // Verificación de las credenciales
+    if (pass == "holaclave" && pin == 2785) {
+        std::cout << "Login exitoso\n";
+    }
+    else {
+        std::cout << "Login fallido\n";
+    }
+
+    return 0;
+    } while (true);
+}
+
+
+
+
+
+
