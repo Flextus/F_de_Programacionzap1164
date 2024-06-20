4bc81972d5c456517f1fcf9b1755700e93a30d37
Flextus <benjasg10@gmail.com>
2024-06-20 12:47:26 -0600

    001_2006_Benjamin
    p18 done

--- 018_memoria_dina/018_memoria_dina.cpp
+++ 018_memoria_dina/018_memoria_dina.cpp
@@ -1,70 +1,120 @@
 #include <iostream>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>
-
+#include <cstdlib> // for srand, rand
+#include <ctime>   // for time
 
 void MATRIZMAS3(int** matriz, int x, int y)
 {
-	
-	
+    srand(time(NULL));
+
+    // Corrección de la condición del bucle
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            matriz[i][j] = rand() % 9 + 1;
+            std::cout << "[" << matriz[i][j] << "]";
+        }
+        std::cout << std::endl; // Añadido para formatear correctamente la salida
+    }
+}
+
+void MATRIZ3(int** matriz, int x, int y)
+{
+    int valuser = 0;
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            std::cout << "Ingresa el valor de la posicion [" << i << "," << j << "]" << std::endl;
+            std::cin >> valuser;
+            matriz[i][j] = valuser;
+        }
+    }
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            std::cout << "[" << matriz[i][j] << "]";
+        }
+        std::cout << std::endl; // Añadido para formatear correctamente la salida
+    }
 }
 
 int main()
 {
-	int mat[2][3], valmat = 0, sw1 = 0, matriz[1][1], x = 0, y = 0;
-	bool cont1 = true;
+    int mat[2][3], valmat = 0, sw1 = 0, x = 0, y = 0;
+    bool cont2 = true;
 
+    std::cout << "Ingresa valor en x\n";
+    std::cin >> x;
+    std::cout << "Ingresa valor en y\n";
+    std::cin >> y;
 
+    // Corrección en la inicialización de la matriz dinámica
+    int** matriz = new int* [x];
+    for (int i = 0; i < x; i++)
+    {
+        matriz[i] = new int[y];
+    }
 
-std::cout << "Selecciona con numeros\n";
-std::cin >> sw1;
+    while (cont2)
+    {
+    tohere:
+        std::cout << "Selecciona con numeros\n";
+        std::cout << "1) 2*3  2) matriz libre\n";
+        std::cin >> sw1;
 
-switch (sw1)
-{
+        switch (sw1)
+        {
+        case 1:
+            for (int i1 = 0; i1 < 2; i1++)
+            {
+                for (int j1 = 0; j1 < 3; j1++)
+                {
+                    std::cout << "Ingresa el valor de la posicion [" << i1 << "," << j1 << "]" << std::endl;
+                    std::cin >> valmat;
+                    mat[i1][j1] = valmat;
+                }
+            }
+            for (int i2 = 0; i2 < 2; i2++)
+            {
+                for (int j2 = 0; j2 < 3; j2++)
+                {
+                    std::cout << "[" << mat[i2][j2] << "]";
+                }
+                std::cout << std::endl; // Simplificación de la salida
+            }
+            break;
 
+        case 2:
+            if (x > 3 || y > 3)
+            {
+                MATRIZMAS3(matriz, x, y);
+            }
+            else
+            {
+                MATRIZ3(matriz, x, y);
+            }
+            break;
 
-case 1:
+        default:
+            std::cout << "Ingresa una opcion Valida\n";
+            goto tohere;
+            break;
+        }
 
-	do
-	{
-		for (int i1 = 0; i1 < 2; i1++)
-		{
-			for (int j1 = 0; j1 < 3; j1++)
-			{
-				std::cout << "Ingresa el valor de la posicion" << "[" << i1 << "," << j1 << "]" << std::endl;
-				std::cin >> valmat;
-				mat[i1][j1] = valmat;
-			}
-		}
-		for (int i2 = 0; i2 < 2; i2++)
-		{
-			for (int j2 = 0; j2 < 3; j2++)
-			{
-				std::cout << "[" << mat[i2][j2] << "]";
-			}
-			std::cout << "" << std::endl;
-		}
-		std::cout << "1 para repetir, 0 next\n";
-		std::cin >> cont1;
-	} while (cont1);
-	//matriz 2*3 con valores de usuario
-	break;
+        std::cout << "1 para repetir, 0 end\n";
+        std::cin >> cont2;
+    }
 
-case 2:
+    // Corrección en la eliminación de la matriz dinámica
+    for (int i = 0; i < x; i++)
+    {
+        delete[] matriz[i];
+    }
+    delete[] matriz;
+    matriz = NULL;
 
-	int** matriz = new int* [x];
-	for (int i = 0; i > x; i++)
-	{
-		matriz[i] = new int[y];
-	}
-
-
-
-
-
-	
-
-	
-}
-
+    return 0;
 }
