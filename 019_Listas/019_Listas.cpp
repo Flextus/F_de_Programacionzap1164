7b41c54805d35896a2fc2c01ccc9e25e055c7031
Flextus <benjasg10@gmail.com>
2024-06-25 21:42:55 -0600

    002_2506_Benjamin
    p019 start

---
+++ 019_Listas/019_Listas.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+
+class Body
+{
+public:
+    std::string part;
+    int numberof;
+    Body* next;
+};
+
+void print()
+{
+
+}
+
+
+int main()
+{
+    Body* one = NULL;
+    Body* two = NULL;
+    Body* three = NULL;
+    Body* four = NULL;
+    Body* five = NULL;
+    Body* six = NULL;
+
+    one = new Body();
+    two = new Body();
+    three = new Body();
+    four = new Body();
+    five = new Body();
+    six = new Body();
+
+    std::cout << sizeof(Body) << std::endl;
+    std::cout << sizeof(one) << std::endl;
+    std::cout << sizeof(six) << std::endl;
+    std::cout << (sizeof(Body) / sizeof(six)) << std::endl;
+    int size1 = (sizeof(Body) / sizeof(one));
+
+    std::cout << "Ingresa la primer parte del cuerpo\n";
+    std::cin >> one->part;
+    std::cout << "Cuantas tienes?\n";
+    std::cin >> one->numberof;
+    one->next = two;
+    std::cout << "Ingresa la segunda parte del cuerpo\n";
+    std::cin >> two->part;
+    std::cout << "Cuantas tienes?\n";
+    std::cin >> two->numberof;
+    two->next = three;
+    std::cout << "Ingresa la tercer parte del cuerpo\n";
+    std::cin >> three->part;
+    std::cout << "Cuantas tienes?\n";
+    std::cin >> three->numberof;
+    three->next = four;
+    std::cout << "Ingresa la cuarta parte del cuerpo\n";
+    std::cin >> four->part;
+    std::cout << "Cuantas tienes?\n";
+    std::cin >> four->numberof;
+    four->next = five;
+    std::cout << "Ingresa la quinta parte del cuerpo\n";
+    std::cin >> five->part;
+    std::cout << "Cuantas tienes?\n";
+    std::cin >> five->numberof;
+    five->next = six;
+    std::cout << "Ingresa la sexta parte del cuerpo\n";
+    std::cin >> six->part;
+    std::cout << "Cuantas tienes?\n";
+    std::cin >> six->numberof;   
+    six->next = NULL;
+
+}
+
